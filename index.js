108362e9306c873cf170c620f42f1f5c5a587291
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-27 12:26:44 -0400

    commit tree 67c577547cce2c57ba5fbc5eb8370bbfa8dea50b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Stephen Ligtenberg",
+    streetAddress: "122 S Roberson"
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    const newEmployeeObject = {...employeeObject};
+    newEmployeeObject[key] = value;
+    return newEmployeeObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    const newEmployeeObject = {...employeeObject};
+    delete newEmployeeObject[key];
+    return newEmployeeObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key];
+    return employeeObject;
+}
